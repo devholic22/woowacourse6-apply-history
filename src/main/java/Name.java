ea429cdf9d38f28dc08c7aae73cce5566c2e750b
devholic22 <hyunjoon.tech@gmail.com>
2023-09-13 23:05:02 +0900

    refactor: Name에서 Name 리스트 생성 및 중복처리를 하도록 변경

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Name {
 
@@ -17,13 +18,22 @@ public class Name {
         return new Name(name);
     }
 
+    public static List<Name> createNameList(String input) {
+        String[] splitNames = input.split(NAME_SPLITTER);
+        return Arrays.stream(splitNames)
+                .filter(Name::validateNameLength)
+                .map(Name::convertStringToName)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
     public static boolean validateNameLength(String name) {
         return name.length() >= MINIMUM_CAR_NAME_LENGTH && name.length() <= MAXIMUM_CAR_NAME_LENGTH;
     }
 
-    public static List<String> createNameList(String input) {
-        String[] splitNames = input.split(NAME_SPLITTER);
-        return Arrays.asList(splitNames);
+    private static Name convertStringToName(String name) {
+        name = name.strip();
+        return Name.from(name);
     }
 
     public boolean isYourName(String name) {
