fd6c1f43c6560039056f4c69430e04515b63b927
devholic22 <hyunjoon.tech@gmail.com>
2023-09-13 16:00:41 +0900

    refactor: RANDOM_RANGE_EXPAND 상수 정의

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,8 +1,9 @@
 public class Dice {
 
     private static final int DICE_MAX_VALUE = 10;
+    private static final int RANDOM_RANGE_EXPAND = 10000;
 
     public static int getRandomValue() {
-        return (int) ((Math.random() * 10000) % DICE_MAX_VALUE);
+        return (int) ((Math.random() * RANDOM_RANGE_EXPAND) % DICE_MAX_VALUE);
     }
 }
