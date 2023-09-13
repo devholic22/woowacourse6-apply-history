47700f2355335ff8d3f7b8056b650e809d218f28
devholic22 <hyunjoon.tech@gmail.com>
2023-09-14 01:03:36 +0900

    test: 이름은 1자 이상 5자 이하여야만 저장된다.

--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -31,4 +31,17 @@ public class NameTest {
         // then
         Assertions.assertThat(nameList.size()).isEqualTo(2);
     }
+
+    @Test
+    @DisplayName("이름은 1자 이상 5자 이하여야만 저장된다.")
+    public void nameLengthTest() {
+        // given
+        String nameInput = "hello12345, test1";
+
+        // when
+        List<Name> nameList = Name.createNameList(nameInput);
+
+        // then
+        Assertions.assertThat(nameList.size()).isEqualTo(1);
+    }
 }
