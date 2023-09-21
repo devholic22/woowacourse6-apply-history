01fc14195a490fcfc9d92b36d01ba105361be4b5
devholic22 <hyunjoon.tech@gmail.com>
2023-09-21 23:08:36 +0900

    refactor: 상수와 객체 간 공백 추가

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -3,6 +3,7 @@ public class Car {
     private static final int START_DISTANCE = 0;
     private static final int ACCELERATE_MINIMUM_VALUE = 4;
     private static final int ACCELERATE_MAXIMUM_VALUE = 9;
+    
     private final Name name;
     private int distance;
 
