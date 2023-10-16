3669d095c8173de79e2816f41be1dca74061603e
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 11:18:42 +0900

    feat: 1번 문제 상수 정의

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,14 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final int LEFT = 0;
+    private static final int RIGHT = 1;
+    private static final int POBI_WIN = 1;
+    private static final int CRONG_WIN = 0;
+    private static final int DRAW = 0;
+    private static final int ERROR = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
