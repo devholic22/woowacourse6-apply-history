850563d46c55950b5dc0fd067195e2c71eb2ccd2
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 20:01:33 +0900

    feat: [5번 문제] 각 금액대로의 값을 담을 리스트 (결과용)를 만든다.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public class Problem5 {
     private static final int[] COINS = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(COINS.length);
         return answer;
     }
 }
