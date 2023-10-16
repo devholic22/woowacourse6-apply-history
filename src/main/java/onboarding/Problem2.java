5ecce9eba25e5743a3bd3484527df060d9154d4d
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 13:26:22 +0900

    feat: [2번 문제] 문자열을 돌면서 연속하며 중복된 곳이 있는지 검사한다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    private static boolean isExistDuplicate(final List<String> tokens) {
+        List<String> tokensWithoutBlank = tokens.stream()
+                .filter(token -> !token.isEmpty())
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < tokensWithoutBlank.size() - 1; i++) {
+            if (tokensWithoutBlank.get(i).equals(tokensWithoutBlank.get(i + 1))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
