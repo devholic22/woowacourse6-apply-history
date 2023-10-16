4373a65df6fa75f3f790be1d8f7549dc2d0d6060
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 13:46:49 +0900

    refactor: [2번 문제] 스트림을 활용하여 함수 들여쓰기를 줄인다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,9 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
@@ -10,15 +12,16 @@ public class Problem2 {
     }
 
     private static boolean isExistDuplicate(final List<String> tokens) {
-        List<String> tokensWithoutBlank = tokens.stream()
+        List<String> tokensWithoutBlank = getValuesWithoutBlank(tokens);
+
+        return IntStream.range(0, tokensWithoutBlank.size() - 1)
+                .anyMatch(i -> tokensWithoutBlank.get(i).equals(tokensWithoutBlank.get(i + 1)));
+    }
+
+
+    private static List<String> getValuesWithoutBlank(final List<String> tokens) {
+        return tokens.stream()
                 .filter(token -> !token.isEmpty())
                 .collect(Collectors.toList());
-
-        for (int i = 0; i < tokensWithoutBlank.size() - 1; i++) {
-            if (tokensWithoutBlank.get(i).equals(tokensWithoutBlank.get(i + 1))) {
-                return true;
-            }
-        }
-        return false;
     }
 }
