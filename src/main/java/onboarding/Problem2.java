47c92ba450339430b5d321a77de9c0a6d6d8a89e
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 14:43:16 +0900

    refactor: [2번 문제] while 이후 for문을 서로 분리하고 나머지 함수도 쪼갠다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,35 +9,57 @@ public class Problem2 {
 
     public static String solution(String cryptogram) {
         List<String> tokens = convertStringToList(cryptogram);
+        while (isExistDuplicate(tokens)) {
+            removeDuplicateToken(tokens);
+        }
+        return convertListToString(tokens);
+    }
+
+    private static void removeDuplicateToken(final List<String> tokens) {
         boolean find = false;
         int start = 0;
         int end = 0;
-        while (isExistDuplicate(tokens)) {
-            for (int i = 0; i < tokens.size() - 1; i++) {
-                if (!isExistDuplicate(tokens)) {
-                    break;
-                }
-                if (tokens.size() == 2 && isBothSame(tokens.get(i), tokens.get(i + 1))) {
-                    tokens.clear();
-                    break;
-                }
-                if (!find && isBothSame(tokens.get(i), tokens.get(i + 1))) {
-                    find = true;
-                    start = i;
-                    end = i + 1;
-                } else if (find && isBothSame(tokens.get(i), tokens.get(i + 1))) {
-                    end = i + 1;
-                } else if (find && !isBothSame(tokens.get(i), tokens.get(i + 1))) {
-                    find = false;
-                    for (int j = start; j <= end; j++) {
-                        tokens.remove(start);
-                    }
-                    start = i + 1;
-                    end = i + 1;
-                }
+
+        for (int i = 0; i < tokens.size() - 1; i++) {
+            if (isOnlyRemainTwoAndBothSame(tokens)) {
+                clearList(tokens);
+                break;
+            }
+            if (!find && isBothSame(tokens.get(i), tokens.get(i + 1))) {
+                find = changeFind(find);
+                start = i;
+                end = i + 1;
+            } else if (find && isBothSame(tokens.get(i), tokens.get(i + 1))) {
+                end = i + 1;
+            } else if (find && !isBothSame(tokens.get(i), tokens.get(i + 1))) {
+                find = changeFind(find);
+                changeValueToBlankFromStartToEnd(tokens, start, end);
+                start = i + 1;
+                end = i + 1;
             }
         }
+    }
+
+    private static boolean changeFind(boolean find) {
+        return !find;
+    }
+
+    private static void clearList(final List<String> tokens) {
+        tokens.clear();
+    }
+
+    private static String convertListToString(final List<String> tokens) {
         return String.join("", tokens);
+    }
+
+    private static void changeValueToBlankFromStartToEnd(final List<String> tokens, final int start, final int end) {
+        for (int j = start; j <= end; j++) {
+            tokens.remove(start);
+        }
+    }
+
+    private static boolean isOnlyRemainTwoAndBothSame(final List<String> tokens) {
+        return tokens.size() == 2 && isBothSame(tokens.get(0), tokens.get(1));
     }
 
     private static boolean isBothSame(final String origin, final String test) {
@@ -50,7 +72,6 @@ public class Problem2 {
         return IntStream.range(0, tokensWithoutBlank.size() - 1)
                 .anyMatch(i -> tokensWithoutBlank.get(i).equals(tokensWithoutBlank.get(i + 1)));
     }
-
 
     private static List<String> getValuesWithoutBlank(final List<String> tokens) {
         return tokens.stream()
