ec04e2467f50bafbb8340cc38a34a2433b94fe39
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 20:14:22 +0900

    fix: [5번 문제] 각 금액대로의 값을 담을 리스트 (결과용)를 만든다. (처음에는 int 배열로 만든다.)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,19 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem5 {
 
     private static final int[] COINS = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>(COINS.length);
-        return answer;
+        int[] answer = new int[COINS.length];
+        return Arrays.stream(answer)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
