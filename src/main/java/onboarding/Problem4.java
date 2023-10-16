b608e8d41aef83497deee1d8eec3aa56e0bd2c4a
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 19:33:32 +0900

    feat: [4번 문제] HashMap을 저장해둔다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,25 @@
 package onboarding;
 
+import java.util.HashMap;
+
 public class Problem4 {
+
+    private static final int A_INDEX = 97;
+    private static final HashMap<String, String> DICTIONARY = new HashMap<>();
+
     public static String solution(String word) {
+        initDictionary();
         String answer = "";
         return answer;
+    }
+
+    private static void initDictionary() {
+        for (char character = 'a'; character <= 'z'; character++) {
+            DICTIONARY.put(String.valueOf(character), String.valueOf(createReverseAlphabet(character)));
+        }
+    }
+
+    private static char createReverseAlphabet(final char character) {
+        return (char) ('z' - (character - A_INDEX));
     }
 }
