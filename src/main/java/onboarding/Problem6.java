55d1155f157d1ccd9258820226da528e0865855f
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 22:30:51 +0900

    feat: [6번 문제] HashMap 형태로 저장하여, 닉네임을 key, 이메일을 value로 설정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem6 {
@@ -7,12 +8,14 @@ public class Problem6 {
     private static final int SAME_CONDITION_VALUE = 2;
     private static final int EMAIL_INDEX = 0;
     private static final int NICKNAME_INDEX = 1;
+    private static final HashMap<String, String> USERS = new HashMap<>();
 
     public static List<String> solution(List<List<String>> forms) {
 
         for (List<String> form : forms) {
             String nickname = form.get(NICKNAME_INDEX);
             String email = form.get(EMAIL_INDEX);
+            USERS.put(nickname, email);
         }
 
         List<String> answer = List.of("answer");
