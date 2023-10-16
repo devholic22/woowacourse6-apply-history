ce07eb07571497c337cd7cafeaec800ba320b47c
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 15:52:37 +0900

    feat: [3번 문제] 박수 치는 수들을 상수화 해 둔다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,13 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
+
+    private static final List<Integer> CLAP_NUMBERS = List.of(3, 6, 9);
+
     public static int solution(int number) {
         int[] numbers = new int[number + 1];
-        int answer = 0;
         return answer;
     }
 }
