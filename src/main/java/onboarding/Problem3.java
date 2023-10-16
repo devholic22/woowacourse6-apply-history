e720045d10a27495eeadf31a62dab4fd781e019c
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 15:31:42 +0900

    feat: [3번 문제] number 길이만큼의 배열을 생성한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,6 +2,7 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        int[] numbers = new int[number + 1];
         int answer = 0;
         return answer;
     }
