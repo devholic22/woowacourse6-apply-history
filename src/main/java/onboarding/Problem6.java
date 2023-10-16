78e83b6c7496f17bd959f1c12cffb5183a221066
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 22:29:58 +0900

    feat: [6번 문제] 각 form에서 이메일과 닉네임을 분리

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -9,6 +9,12 @@ public class Problem6 {
     private static final int NICKNAME_INDEX = 1;
 
     public static List<String> solution(List<List<String>> forms) {
+
+        for (List<String> form : forms) {
+            String nickname = form.get(NICKNAME_INDEX);
+            String email = form.get(EMAIL_INDEX);
+        }
+
         List<String> answer = List.of("answer");
         return answer;
     }
