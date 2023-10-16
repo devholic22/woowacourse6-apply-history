3cd031ad716e4f5ba01e59303d8068c698ab6509
devholic22 <hyunjoon.tech@gmail.com>
2023-10-16 22:28:32 +0900

    feat: [6번 문제] 이메일, 닉네임 인덱스 상수화

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Problem6 {
 
     private static final int SAME_CONDITION_VALUE = 2;
+    private static final int EMAIL_INDEX = 0;
+    private static final int NICKNAME_INDEX = 1;
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
