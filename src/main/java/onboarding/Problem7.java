ffca862681f711f076fe26e6640fab80a0e4964b
devholic22 <hyunjoon.tech@gmail.com>
2023-10-17 09:36:48 +0900

    feat: [7번 문제] 정렬 조건이 여러 개 있으므로, 사람을 저장할 때 Person 클래스로 만들어둔다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,7 +4,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
-    public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+
+    private class Person {
+
+        private int score;
+        private String name;
+
+        public Person(final int score, final String name) {
+            this.score = score;
+            this.name = name;
+        }
+    }
+
+        public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
