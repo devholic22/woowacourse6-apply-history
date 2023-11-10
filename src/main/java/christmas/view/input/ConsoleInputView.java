ad7a09ffc7ddc34623812d16d44d9ec1cb6206f5
devholic22 <hyunjoon.tech@gmail.com>
2023-11-11 02:06:05 +0900

    feat: 입력 뷰 정의
    콘솔 환경에서의 입력을 관리할 입력 뷰 정의, 구현

---
+++ src/main/java/christmas/view/input/ConsoleInputView.java
@@ -0,0 +1,11 @@
+package christmas.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleInputView implements InputView {
+
+    @Override
+    public String readLine() {
+        return Console.readLine();
+    }
+}
