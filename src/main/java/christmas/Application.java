258fef1bb0665045cecef0ed9b2a34b66bc198db
devholic22 <hyunjoon.tech@gmail.com>
2023-11-11 02:09:16 +0900

    feat: 컨트롤러 정의
    - 입출력 뷰를 관리하는 컨트롤러 정의
    - 기본 호출 메서드 정의

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,18 @@
 package christmas;
 
+import christmas.controller.EventController;
+import christmas.view.input.ConsoleInputView;
+import christmas.view.input.InputView;
+import christmas.view.output.ConsoleOutputView;
+import christmas.view.output.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new ConsoleInputView();
+        OutputView outputView = new ConsoleOutputView();
+        EventController controller = new EventController(inputView, outputView);
+
+        controller.start();
     }
 }
---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,19 @@
+package christmas.controller;
+
+import christmas.view.input.InputView;
+import christmas.view.output.OutputView;
+
+public class EventController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public EventController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+
+    }
+}
