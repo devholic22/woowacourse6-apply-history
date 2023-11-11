e644bbbe3959cab0d4408a95ad0d63961432d40c
devholic22 <hyunjoon.tech@gmail.com>
2023-11-12 00:23:43 +0900

    test: BonusManager 관련 테스트 작성
    금액에 따른 증정 이벤트 수령 여부 테스트

---
+++ src/test/java/christmas/model/BonusManagerTest.java
@@ -0,0 +1,37 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.model.order.Order;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class BonusManagerTest {
+
+    @Test
+    @DisplayName("증정 이벤트 기능 테스트 - 없음")
+    void emptyBonusTest() {
+        // given
+        int cost = 100_000;
+
+        // when
+        List<Order> bonusOrders = BonusManager.giveBonusOrders(cost);
+
+        // then
+        assertThat(bonusOrders).isEmpty();
+    }
+
+    @Test
+    @DisplayName("증정 이벤트 기능 테스트 - 지급 완료")
+    void receiveBonusTest() {
+        // given
+        int cost = 150_000;
+
+        // when
+        List<Order> bonusOrders = BonusManager.giveBonusOrders(cost);
+
+        // then
+        assertThat(bonusOrders).isNotEmpty();
+    }
+}
