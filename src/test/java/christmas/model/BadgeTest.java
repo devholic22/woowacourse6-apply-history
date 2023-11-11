fc2abf8793b7886c02b3ab97a1e60374d1d6a25f
devholic22 <hyunjoon.tech@gmail.com>
2023-11-12 00:16:11 +0900

    test: Badge 관련 테스트 작성
    Badge 조회 테스트 작성

---
+++ src/test/java/christmas/model/BadgeTest.java
@@ -0,0 +1,51 @@
+package christmas.model;
+
+import static christmas.model.Badge.NOT_THING;
+import static christmas.model.Badge.SANTA;
+import static christmas.model.Badge.TREE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BadgeTest {
+
+    @Test
+    @DisplayName("Badge 조회 테스트 - 없음")
+    void notFoundBadgeFindTest() {
+        // given
+        int cost = 3_000;
+
+        // when
+        Badge findBadge = Badge.findByCost(cost);
+
+        // then
+        assertThat(findBadge).isEqualTo(NOT_THING);
+    }
+
+    @Test
+    @DisplayName("Badge 조회 테스트 - 트리")
+    void treeBadgeFindTest() {
+        // given
+        int cost = 12_000;
+
+        // when
+        Badge findBadge = Badge.findByCost(cost);
+
+        // then
+        assertThat(findBadge).isEqualTo(TREE);
+    }
+
+    @Test
+    @DisplayName("Badge 조회 테스트 - 산타")
+    void santaBadgeFindTest() {
+        // given
+        int cost = 150_000;
+
+        // when
+        Badge findBadge = Badge.findByCost(cost);
+
+        // then
+        assertThat(findBadge).isEqualTo(SANTA);
+    }
+}
