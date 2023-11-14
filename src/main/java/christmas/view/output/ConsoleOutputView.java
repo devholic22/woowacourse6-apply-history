d29b04b29cbb2413fd5a9e7598fcdc26c4e45991
devholic22 <hyunjoon.tech@gmail.com>
2023-11-15 01:51:43 +0900

    refactor: 출력 뷰 StringBuilder 적용
    - 출력 시 StringBuilder를 사용하도록 수정
    - printOrderedMenus, printGiftOrders 등은 핵심 내용만 StringBuilder를 이용
    - 반복적으로 쓰이는 단어를 상수화

--- src/main/java/christmas/view/output/ConsoleOutputView.java
+++ src/main/java/christmas/view/output/ConsoleOutputView.java
@@ -8,6 +8,8 @@ public class ConsoleOutputView implements OutputView {
 
     private static final String SIZE_MARK = "개";
     private static final String MONEY_REGEX = "%,d";
+    private static final String WORD_DIVIDER = " ";
+    private static final String NEW_LINE = "\n";
     private static final String CURRENCY = "원";
     private static final int EMPTY_MONEY = 0;
     private static final int PROMOTION_SIGN = -1;
@@ -34,7 +36,12 @@ public class ConsoleOutputView implements OutputView {
 
     @Override
     public void printOrderDay(final int day) {
-        System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        StringBuilder builder = new StringBuilder();
+        builder.append("12월 ")
+                .append(day)
+                .append("일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+
+        System.out.println(builder);
     }
 
     @Override
@@ -42,15 +49,30 @@ public class ConsoleOutputView implements OutputView {
         System.out.println();
         System.out.println("<주문 메뉴>");
         for (OrderResponse order : orders) {
-            System.out.println(order.name() + " " + order.size() + SIZE_MARK);
+            System.out.println(convertOrderAnswer(order));
         }
+    }
+
+    private String convertOrderAnswer(final OrderResponse order) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(order.name())
+                .append(WORD_DIVIDER)
+                .append(order.size())
+                .append(SIZE_MARK);
+
+        return builder.toString();
     }
 
     @Override
     public void printCostBeforePromotion(final int cost) {
-        System.out.println();
-        System.out.println("<할인 전 총주문 금액>");
-        System.out.println(String.format(MONEY_REGEX, cost) + CURRENCY);
+        StringBuilder builder = new StringBuilder();
+        builder.append(NEW_LINE)
+                .append("<할인 전 총주문 금액>")
+                .append(NEW_LINE)
+                .append(String.format(MONEY_REGEX, cost))
+                .append(CURRENCY);
+
+        System.out.println(builder);
     }
 
     @Override
@@ -63,8 +85,18 @@ public class ConsoleOutputView implements OutputView {
             return;
         }
         for (OrderResponse giftOrder : giftOrders) {
-            System.out.println(giftOrder.name() + " " + giftOrder.size() + SIZE_MARK);
+            System.out.println(convertGiftOrderAnswer(giftOrder));
         }
+    }
+
+    private String convertGiftOrderAnswer(final OrderResponse giftOrder) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(giftOrder.name())
+                .append(WORD_DIVIDER)
+                .append(giftOrder.size())
+                .append(SIZE_MARK);
+
+        return builder.toString();
     }
 
     private void printEmpty() {
@@ -85,7 +117,13 @@ public class ConsoleOutputView implements OutputView {
     }
 
     private String convertPromotionAnswer(final PromotionResponse promotion) {
-        return promotion.name() + ": " + String.format(MONEY_REGEX, PROMOTION_SIGN * promotion.cost()) + CURRENCY;
+        StringBuilder builder = new StringBuilder();
+        builder.append(promotion.name())
+                .append(": ")
+                .append(String.format(MONEY_REGEX, PROMOTION_SIGN * promotion.cost()))
+                .append(CURRENCY);
+
+        return builder.toString();
     }
 
     @Override
@@ -93,27 +131,47 @@ public class ConsoleOutputView implements OutputView {
         if (cost == EMPTY_MONEY) {
             return;
         }
-        System.out.println("증정 이벤트: " + String.format(MONEY_REGEX, (PROMOTION_SIGN * cost)) + CURRENCY);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("증정 이벤트: ")
+                .append(String.format(MONEY_REGEX, (PROMOTION_SIGN * cost)))
+                .append(CURRENCY);
+
+        System.out.println(builder);
     }
 
     @Override
     public void printTotalBenefitCost(final int cost) {
-        System.out.println();
-        System.out.println("<총혜택 금액>");
-        System.out.println(String.format(MONEY_REGEX, PROMOTION_SIGN * cost) + CURRENCY);
+        StringBuilder builder = new StringBuilder();
+        builder.append(NEW_LINE)
+                .append("<총혜택 금액>")
+                .append(NEW_LINE)
+                .append(String.format(MONEY_REGEX, PROMOTION_SIGN * cost))
+                .append(CURRENCY);
+
+        System.out.println(builder);
     }
 
     @Override
     public void printCostAfterDiscount(final int cost) {
-        System.out.println();
-        System.out.println("<할인 후 예상 결제 금액>");
-        System.out.println(String.format(MONEY_REGEX, cost) + CURRENCY);
+        StringBuilder builder = new StringBuilder();
+        builder.append(NEW_LINE)
+                .append("<할인 후 예상 결제 금액>")
+                .append(NEW_LINE)
+                .append(String.format(MONEY_REGEX, cost))
+                .append(CURRENCY);
+
+        System.out.println(builder);
     }
 
     @Override
     public void printBadge(final String badgeName) {
-        System.out.println();
-        System.out.println("<12월 이벤트 배지>");
-        System.out.println(badgeName);
+        StringBuilder builder = new StringBuilder();
+        builder.append(NEW_LINE)
+                .append("<12월 이벤트 배지>")
+                .append(NEW_LINE)
+                .append(badgeName);
+
+        System.out.println(builder);
     }
 }
