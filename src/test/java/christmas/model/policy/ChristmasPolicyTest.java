9e523553fff236d50cea55492a08962e70c12824
devholic22 <hyunjoon.tech@gmail.com>
2023-11-15 03:21:49 +0900

    test: ChristmasPolicyTest 수정
    - 할인 적용 예외 검증에 가격 미달 추가
    - 날짜가 지났을 때 (기존에 작성한)와 같으므로, CsvSource를 활용하여 단축

--- src/test/java/christmas/model/policy/ChristmasPolicyTest.java
+++ src/test/java/christmas/model/policy/ChristmasPolicyTest.java
@@ -7,6 +7,8 @@ import christmas.model.order.Orders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ChristmasPolicyTest {
 
@@ -33,12 +35,16 @@ class ChristmasPolicyTest {
         assertThat(discountPolicy.canDiscount(requestDay, orders)).isTrue();
     }
 
-    @Test
-    @DisplayName("크리스마스 디데이 할인 - 지원 안 됨")
-    void notSupportDiscountTest() {
+    @ParameterizedTest(name = "날짜가 {0}일이고 메뉴 주문이 [{1}]인 경우")
+    @CsvSource(value = {
+            "26, 제로콜라-2,초코케이크-1",
+            "10, 타파스-1,제로콜라-1"
+    }, delimiterString = ", ")
+    @DisplayName("날짜가 지나거나 가격이 미달될 시 할인이 적용되지 않는다.")
+    void notDiscountTest(final String dayInput, final String ordersInput) {
         // given
-        Day requestDay = Day.from("26");
-        Orders orders = Orders.from("제로콜라-2,초코케이크-1");
+        Day requestDay = Day.from(dayInput);
+        Orders orders = Orders.from(ordersInput);
 
         // when
         int discountCost = discountPolicy.discount(requestDay, orders);
