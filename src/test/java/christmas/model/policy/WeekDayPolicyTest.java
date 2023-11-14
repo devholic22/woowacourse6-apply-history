9a8bf62cb8821222301812e8818fd1a3be3bab89
devholic22 <hyunjoon.tech@gmail.com>
2023-11-15 03:36:47 +0900

    test: WeekDayPolicyTest 수정
    - 할인 예외 테스트 시 가격 미달 테스트 추가
    - 중복되는 코드를 CsvSource를 활용하여 제거

--- src/test/java/christmas/model/policy/WeekDayPolicyTest.java
+++ src/test/java/christmas/model/policy/WeekDayPolicyTest.java
@@ -7,6 +7,8 @@ import christmas.model.order.Orders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class WeekDayPolicyTest {
 
@@ -33,27 +35,17 @@ class WeekDayPolicyTest {
         assertThat(discountPolicy.canDiscount(requestDay, orders)).isTrue();
     }
 
-    @Test
-    @DisplayName("평일 할인 - 평일이 아닐 시 지원 안 됨")
-    void notWeekDayTest() {
+    @ParameterizedTest(name = "날짜가 {0}일이고 메뉴 주문이 [{1}]인 경우")
+    @CsvSource(value = {
+            "1, 제로콜라-2,초코케이크-1",
+            "3, 제로콜라-2,바비큐립-1",
+            "3, 아이스크림-1"
+    }, delimiterString = ", ")
+    @DisplayName("평일 할인 - 평일이 아니거나, 디저트가 없거나, 가격이 미달일 시 할인이 적용되지 않는다.")
+    void notWeekDayTest(final String dayInput, final String ordersInput) {
         // given
-        Day requestDay = Day.from("1");
-        Orders orders = Orders.from("제로콜라-2,초코케이크-1");
-
-        // when
-        int discountCost = discountPolicy.discount(requestDay, orders);
-
-        // then
-        assertThat(discountCost).isEqualTo(0);
-        assertThat(discountPolicy.canDiscount(requestDay, orders)).isFalse();
-    }
-
-    @Test
-    @DisplayName("평일 할인 - 디저트가 없을 시 지원 안 됨")
-    void notInDessertTest() {
-        // given
-        Day requestDay = Day.from("3");
-        Orders orders = Orders.from("제로콜라-2,바비큐립-1");
+        Day requestDay = Day.from(dayInput);
+        Orders orders = Orders.from(ordersInput);
 
         // when
         int discountCost = discountPolicy.discount(requestDay, orders);
