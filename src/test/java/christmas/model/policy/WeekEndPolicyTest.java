e9e2aa98df357ad231c437a323ce76a925d5bf52
devholic22 <hyunjoon.tech@gmail.com>
2023-11-15 03:43:29 +0900

    test: WeekEndPolicyTest 수정
    - CsvSource를 활용하여 중복 코드 삭제
    - 날짜를 직접 작성하도록 수정

--- src/test/java/christmas/model/policy/WeekEndPolicyTest.java
+++ src/test/java/christmas/model/policy/WeekEndPolicyTest.java
@@ -7,11 +7,10 @@ import christmas.model.order.Orders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class WeekEndPolicyTest {
-
-    private static final String WEEK_END = "1";
-    private static final String WEEK_DAY = "3";
 
     private DiscountPolicy discountPolicy;
 
@@ -24,7 +23,7 @@ class WeekEndPolicyTest {
     @DisplayName("주말 할인 정상")
     void weekEndDiscountTest() {
         // given
-        Day requestDay = Day.from(WEEK_END);
+        Day requestDay = Day.from("1");
         Orders orders = Orders.from("제로콜라-2,초코케이크-1,바비큐립-1");
         int expectedCost = 2_023;
 
@@ -36,27 +35,16 @@ class WeekEndPolicyTest {
         assertThat(discountPolicy.canDiscount(requestDay, orders)).isTrue();
     }
 
-    @Test
-    @DisplayName("주말 할인 - 주말이 아닐 시 지원 안 됨")
-    void notWeekEndTest() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "3, 제로콜라-2,초코케이크-1,바비큐립-1",
+            "1, 제로콜라-2,초코케이크-3"
+    }, delimiterString = ", ")
+    @DisplayName("주말이 아니거나 메인 메뉴가 없을 시 할인이 적용되지 않는다.")
+    void notWeekEndTest(final String dayInput, final String ordersInput) {
         // given
-        Day requestDay = Day.from(WEEK_DAY);
-        Orders orders = Orders.from("제로콜라-2,초코케이크-1,바비큐립-1");
-
-        // when
-        int discountCost = discountPolicy.discount(requestDay, orders);
-
-        // then
-        assertThat(discountCost).isEqualTo(0);
-        assertThat(discountPolicy.canDiscount(requestDay, orders)).isFalse();
-    }
-
-    @Test
-    @DisplayName("주말 할인 - 메인 메뉴가 없을 시 지원 안 됨")
-    void notInMainDishTest() {
-        // given
-        Day requestDay = Day.from(WEEK_END);
-        Orders orders = Orders.from("제로콜라-2,초코케이크-3");
+        Day requestDay = Day.from(dayInput);
+        Orders orders = Orders.from(ordersInput);
 
         // when
         int discountCost = discountPolicy.discount(requestDay, orders);
