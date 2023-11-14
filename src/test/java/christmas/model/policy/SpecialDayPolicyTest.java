aac98d1bc8579f138041cc15b527aaa0e42d74d2
devholic22 <hyunjoon.tech@gmail.com>
2023-11-15 03:27:12 +0900

    test: SpecialDayPolicyTest 수정
    - 특별 날짜들의 입력에 대해 전부 통과되는지를 검증하기 위해 CsvSource 적용
    - 기존에 검증했던 특별 할인 지원이 안 될 때 이유 추가 작성

--- src/test/java/christmas/model/policy/SpecialDayPolicyTest.java
+++ src/test/java/christmas/model/policy/SpecialDayPolicyTest.java
@@ -7,6 +7,8 @@ import christmas.model.order.Orders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class SpecialDayPolicyTest {
 
@@ -17,11 +19,12 @@ class SpecialDayPolicyTest {
         discountPolicy = new SpecialDayPolicy();
     }
 
-    @Test
+    @ParameterizedTest(name = "날짜가 {0}일인 경우")
+    @CsvSource(value = {"3", "10", "17", "24", "25", "31"})
     @DisplayName("특별 할인 정상")
-    void specialDiscountTest() {
+    void specialDiscountTest(final String dayInput) {
         // given
-        Day requestDay = Day.from("3");
+        Day requestDay = Day.from(dayInput);
         Orders orders = Orders.from("바비큐립-1,티본스테이크-2");
         int expectedCost = 1_000;
 
@@ -34,7 +37,7 @@ class SpecialDayPolicyTest {
     }
 
     @Test
-    @DisplayName("특별 할인 - 지원 안 됨")
+    @DisplayName("특별 할인 - 지원 안 됨 (특별한 날이 아님)")
     void notSupportDiscountTest() {
         // given
         Day requestDay = Day.from("1");
