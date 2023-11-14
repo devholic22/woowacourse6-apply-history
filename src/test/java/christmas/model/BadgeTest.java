268c12793a50851a14b5aee1b2e9661a7265b2f0
devholic22 <hyunjoon.tech@gmail.com>
2023-11-15 02:56:24 +0900

    test: BadgeTest 수정
    CsvSource를 활용하여 중복 코드를 제거하도록 수정

--- src/test/java/christmas/model/BadgeTest.java
+++ src/test/java/christmas/model/BadgeTest.java
@@ -1,51 +1,26 @@
 package christmas.model;
 
-import static christmas.model.Badge.NOT_THING;
-import static christmas.model.Badge.SANTA;
-import static christmas.model.Badge.TREE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BadgeTest {
 
-    @Test
-    @DisplayName("Badge 조회 - 없음")
-    void notFoundBadgeFindTest() {
-        // given
-        int cost = 3_000;
-
+    @ParameterizedTest(name = "{0}원이 들어올 때 {1} Badge인가?")
+    @CsvSource({
+            "3000, NOT_THING",
+            "6000, STAR",
+            "12000, TREE",
+            "25000, SANTA"
+    })
+    @DisplayName("각 입력값에 따른 적절한 Badge 조회")
+    void findEachBadgeByCostTest(final int cost, final Badge badge) {
         // when
         Badge findBadge = Badge.findByCost(cost);
 
         // then
-        assertThat(findBadge).isEqualTo(NOT_THING);
-    }
-
-    @Test
-    @DisplayName("Badge 조회 - 트리")
-    void treeBadgeFindTest() {
-        // given
-        int cost = 12_000;
-
-        // when
-        Badge findBadge = Badge.findByCost(cost);
-
-        // then
-        assertThat(findBadge).isEqualTo(TREE);
-    }
-
-    @Test
-    @DisplayName("Badge 조회 - 산타")
-    void santaBadgeFindTest() {
-        // given
-        int cost = 150_000;
-
-        // when
-        Badge findBadge = Badge.findByCost(cost);
-
-        // then
-        assertThat(findBadge).isEqualTo(SANTA);
+        assertThat(findBadge).isEqualTo(badge);
     }
 }
